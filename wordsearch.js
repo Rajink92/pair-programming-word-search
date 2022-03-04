343a4e646195c7ba4a2f4e1d3a241428214ae095
Rajin <rajink92@gmail.com>
2022-03-04 14:34:26 -0500

    experiment with vertical flip

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,10 @@
 const wordSearch = (letters, word) => { 
     if (letters.length < 1 || word.length < 1)  return false;
     const horizontalJoin = letters.map(ls => ls.join(''));
-    const vertJoin = letters[0].map((col,index))
-        
+    const vertJoin = letters[0].map((col, index)) =>
+        letters.map((row) => row[index])
+    );
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
