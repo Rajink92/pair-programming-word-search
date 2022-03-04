f797558fa406587417a8dd542db377576dd06d41
Rajin <rajink92@gmail.com>
2022-03-04 14:48:17 -0500

    debug, test pass

--- wordsearch.js
+++ wordsearch.js
@@ -1,13 +1,18 @@
 const wordSearch = (letters, word) => { 
-    if (letters.length < 1 || word.length < 1)  return false;
+    if (letters.length < 1 || word.length < 1)   return false;
     const horizontalJoin = letters.map(ls => ls.join(''));
-    const vertJoin = letters[0].map((col, index)) =>
+    const vertJoin = letters[0].map((col, index) =>
         letters.map((row) => row[index])
     );
-
+    const vertFlip = vertJoin.map((ls) => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
-
-module.exports = wordSearch
+        if (l.includes(word)) return true;
+        else {
+            for (l of vertFlip) {
+              if (l.includes(word)) return true;
+            }
+        }
+     }
+    return false;
+};
+module.exports = wordSearch;
